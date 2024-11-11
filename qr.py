38621d6cfe73b912864b3b5808ebcbecbe73dee0
iniyavan <foriniyavan@gmail.com>
2024-11-11 13:33:00 +0530

    up

---
+++ qr.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Input text
+text = "https://www.example.com"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(text)   
+
+qr.make(fit=True)
+
+# Create an image from the QR code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image   
+
+img.save("qrcode.png")
